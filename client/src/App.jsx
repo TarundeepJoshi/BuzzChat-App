8d8ce15604fbdce8a10ba4dcca07e67cc27b4388
Tarundeep Joshi <tarundeepjoshi0002@gmail.com>
2025-01-05 19:00:36 +0530

    commit client folder

---
+++ client/src/App.jsx
@@ -0,0 +1,18 @@
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import Chat from "./pages/chat";
+import Profile from "./pages/profile";
+import Auth from "./pages/auth";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/auth" element={<Auth />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/chat" element={<Chat />} />
+
+        <Route path="*" element={<Navigate to="/auth" />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
